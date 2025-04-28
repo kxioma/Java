f8e058fd96a94697f657dda81f065d3e126a53cc
kxioma <karen.calderonn@campusucc.edu.co>
2025-04-28 15:20:52 +0000

    CORRECCION ERROR

--- Clase10/EjecutarBanco.java
+++ Clase10/EjecutarBanco.java
@@ -5,12 +5,14 @@ public class EjecutarBanco {
         //creacion de los objetos
 
        
-        Cuenta objCuenta =new Cuenta(numeroCuenta: 1000235, saldo: 2345); 
-        Cliente objCliente =new Cliente(cedula: 1117234567);
+
+        Cuenta objCuenta =new Cuenta(1000235, 2345); 
+        Cliente objCliente =new Cliente(1117234567);
+        
 
         System.out.println("objcuenta");
         System.out.println("objcliente");
-        Banco objBanco = new Banco(id:1, nombre:"Mi primer ahorro", objCuenta);
+        Banco objBanco = new Banco(1, "Mi primer ahorro", objCuenta);
 
         System.out.println("objBanco");
         objBanco.mostrarCliente(objCliente);
