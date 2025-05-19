a45a3af0346f2fc3b4d736afe060e3a469cd139d
kxioma <karen.calderonn@campusucc.edu.co>
2025-05-19 12:50:04 +0000

    nuevo

---
+++ Motos/BatallaMotos.java
@@ -0,0 +1,24 @@
+package Motos;
+
+public class BatallaMotos{
+ private Motos moto1;
+ private Motos moto2;
+
+ public BatallaMotos(Moto m2,  Moto m2){
+
+this.moto1 =  m1;
+this.moto2 = m2;
+ }
+
+
+  public void iniciarpelea(){
+            System.out.println("¡Comienza la batalla entre " + 1.getNombre() + " y " + dino2.getNombre() + "!");
+  }
+
+
+}
+ 
+
+   
+    
+
